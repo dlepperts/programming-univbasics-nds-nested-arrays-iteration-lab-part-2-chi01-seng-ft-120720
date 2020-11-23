9c116778b0debf70bb04260a781b784286cbc5c2
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-23 20:48:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0
     smallest_number_element = 0
-    while element_index > src[row_index].count do
+    while element_index < src[row_index].count do
       if src[row_index][element_index] > smallest_number_element
         smallest_number_element = src[row_index][element_index]
       end
